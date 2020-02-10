3604473890c719aa19f58cd4b2559273bd980f3c
Student <example@example.com>
2020-02-10 11:17:43 -0800

    add initial files and add header.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import Header from "./components/Header";
+// import Feed from "./components/Feed";
+// import Profile from "./components/Profile";
+// import Suggestions from "./components/Suggestions";
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+  
     </div>
   );
 }
---
+++ src/components/Profile.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Profile(){
+  return (
+    <Ticket
+      location="3A"
+      names="Thato and Haley"
+      issue="Firebase will not save record!"/>
+  );
+}
+
+export default TicketList;
