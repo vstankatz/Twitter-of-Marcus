864e227382f22c71967f2e10db3ead888013d28f
marcusjensen15 <marcusjensen1515@gmail.com>
2020-02-17 16:16:03 -0800

    likes now increment up 1 when button is clicked

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -2,19 +2,41 @@ import React from 'react';
 import {ListGroup, Image }from 'react-bootstrap';
 import PropTypes from 'prop-types';
 
-function Tweet(props){
-  return(
-    <div>
-      <ListGroup.Item>
-        <Image className='feedImage' src="https://www.netactivity.us/backup2019/wp-content/uploads/2018/08/Network-Profile.png"></Image>
-        <h5>{props.author}</h5>
-        <p>{props.body}</p>
-      </ListGroup.Item>
-    </div>
-  );
-}
-Tweet.propTypes = {
-  author: PropTypes.string.isRequired,
-  body: PropTypes.string.isRequired
-}
-export default Tweet;
+class Tweet extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+
+      count: 0
+    }
+    this.incrementLikes = this.incrementLikes.bind(this)
+
+    };
+
+    incrementLikes = () => {
+      let newCount = this.state.count +1
+      this.setState({ count: newCount})
+
+    }
+
+    render () {
+      return(
+        <div>
+          <ListGroup.Item>
+            <Image className='feedImage' src="https://www.netactivity.us/backup2019/wp-content/uploads/2018/08/Network-Profile.png"></Image>
+            <h5>{this.props.author}</h5>
+            <p>{this.props.body}</p>
+            <p> Number of likes: {this.state.count}</p>
+            <button onClick={this.incrementLikes}>Click Me</button>
+          </ListGroup.Item>
+        </div>
+      )
+    }
+  }
+
+  Tweet.propTypes = {
+    author: PropTypes.string.isRequired,
+    body: PropTypes.string.isRequired
+  }
+  export default Tweet;
